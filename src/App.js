bbf812c9aebc995069c6543f8deccd62c03fd5e3
athuls1195 <athulsukumaran@gmail.com>
2024-03-17 09:40:12 +0530

    Footer_Context Changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignUp from './Pages/LoginSignUp';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kids_banner from './Components/Assets/banner_kids.png';
 
 function App() {
   return (
@@ -14,9 +17,9 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Shop/>}/>
-          <Route path='/mens' element={<ShopCategory category="men"/>}/>
-          <Route path='/womens' element={<ShopCategory category="women"/>}/>
-          <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+          <Route path='/mens' element={<ShopCategory banner ={men_banner} category="men"/>}/>
+          <Route path='/womens' element={<ShopCategory banner ={women_banner} category="women"/>}/>
+          <Route path='/kids' element={<ShopCategory banner ={kids_banner} category="kid"/>}/>
           <Route path='/product' element={<Product/>}/>
             <Route path=':productId' element={<Product/>}>
           </Route>

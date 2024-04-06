5ba0b988bd3f81d22f6aa2257584cd99266cb5ae
athuls1195 <athulsukumaran@gmail.com>
2024-04-06 11:57:26 +0530

    final app

--- src/Components/Breadcrumbs/Breadcrumb.jsx
+++ src/Components/Breadcrumbs/Breadcrumb.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Breadcrumb.css';
-import arrow_icon from '../Assets/arrow.png'
+import breadcrum_arrow from '../Assets/breadcrum_arrow.png'
 function Breadcrumb(props) {
     const {product} =props;
   return (  
   <div className="breadcrumb">HOME
-  <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" />{product.category} <img src={arrow_icon} alt="" />{product.name}
+  <img src={breadcrum_arrow} alt="" /> SHOP <img src={breadcrum_arrow} alt="" />{product.category} <img src={breadcrum_arrow} alt="" />{product.name}
   </div>
   )  
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
 export const Navbar = () => {
     const [menu,setMenu]=useState("shop");
+    const {getTotalsCartItems} = useContext(ShopContext);
+    console.log(getTotalsCartItems());
   return (
     <div className='navbar'>
         <div className='nav-logo'>
@@ -25,7 +28,7 @@ export const Navbar = () => {
             <Link to='/cart'>
                 <img src={cart_icon} alt="" />
             </Link>
-            <div className="nav-cart-count">0</div>
+            <div className="nav-cart-count">{getTotalsCartItems()}</div>
         </div>
     </div>
   )
